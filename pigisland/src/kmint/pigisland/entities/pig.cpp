18414caa731c2ba23656300c9936aaf9eb6f4bd1
Thijs <ttleeuw96@gmail.com>
2020-12-26 11:56:58 +0100

    they move

--- pigisland/src/kmint/pigisland/entities/pig.cpp
+++ pigisland/src/kmint/pigisland/entities/pig.cpp
@@ -16,20 +16,21 @@ namespace kmint {
         void pig::act(delta_time dt) {
             t_passed_ += dt;
             if (to_seconds(t_passed_) >= 1) {
-                //auto steeringForce = steeringBehaviour.calculate(*this);
+                auto steeringForce = steeringBehaviour.calculate(*this);
 
-                ////Acceleration = Force/Mass
-                //this->setAcceleration(steeringForce);
-                ////update velocity_
-                //this->setVelocity(steeringForce);
+                //Acceleration = Force/Mass
+                kmint::math::vector2d acceleration = steeringForce / mass();
+                //update velocity_
+                velocity += acceleration * to_seconds(dt);
 
-                //this->setPosition(dt);
+                velocity = truncate(velocity, maxSpeed());
 
-                //auto x = kmint::math::vector2d{ this->location().x(), this->location().y() };
-                //auto r = x + this->position;
-                //this->location(r);
-                ////update the position
-                ////move(velocity_ * to_seconds(dt));
+                auto x = kmint::math::vector2d{ this->location().x(), this->location().y() };
+                auto r = x + this->position;
+                this->location(r);
+                //update the position
+
+                location(location() + (velocity * to_seconds(dt)));
 
 
                 t_passed_ = from_seconds(0);
